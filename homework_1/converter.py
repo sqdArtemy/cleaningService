9f40e121252d772ee923df7a91284fc90685a8b1
Artyom Tashyan <squiman15@gmail.com>
2022-04-27 13:33:02 +0500

    homework_1

--- homework_1/converter.py
+++ homework_1/converter.py
@@ -1,6 +1,4 @@
-import abc, json #  import needed libraries
-from  json import JSONDecodeError
-
+import abc #  import needed libraries
 
 class AbstractConverter(abc.ABC): #  abstract class for converter
     @abc.abstractmethod
@@ -27,7 +25,7 @@ class CsvToJson(AbstractConverter): #  converter from CSV to JSON
                         for key in data: content[key.replace("\n", "")] = ""
                     else:
                         i = 0
-                        for key in content: content[key] = data[i].replace("\n", ""); i+=1
+                        for key in content: content[key.replace("\n", "")] = data[i].replace("\n", ""); i+=1
                         converted.append(dict(content)) #  append values of dictionary, not a pointer of it
                     counter += 1
                 if len(converted) <= 1: converted = converted[0];
@@ -39,13 +37,8 @@ class CsvToJson(AbstractConverter): #  converter from CSV to JSON
 class JsonToCsv(AbstractConverter): #  Converter from JSON to CSV
     def converter(self, input, output):
         with open(input, "r") as json_file:
-            data = {}
             result = str()
-
-            try:
-                data = json.load(json_file)
-            except JSONDecodeError:  # Handling some error cases
-                print("I suppose, that file is empty :(")
+            data = eval(json_file.read().replace('\n', '')) # reading json file to dict
 
             for i in data:
                 dictionaries = data[i]
@@ -54,5 +47,5 @@ class JsonToCsv(AbstractConverter): #  Converter from JSON to CSV
                 for j in records: result += (",".join(j) + "\n")
             file_writer(data = result, file = output) #  calling function that writes to a file
 
-obj1 = CsvToJson(); obj1.converter("input_1.csv", "output_1.json")
-obj2 = JsonToCsv(); obj2.converter("input_2.json", "output_2.csv")
+obj1 = CsvToJson(); obj1.converter("input_1.csv", "output_1.json") #  CSV to JSON
+obj2 = JsonToCsv(); obj2.converter("input_2.json", "output_2.csv") #  JSON to CSV
